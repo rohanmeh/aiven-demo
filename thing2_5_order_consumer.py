ebd761c354a804232adee828443adc7f784b5435
Rohan Mehta <rohan.mehta@accenture.com>
2023-06-05 10:37:16 -0400

    Initial commit

---
+++ thing2_5_order_consumer.py
@@ -0,0 +1,18 @@
+from kafka import KafkaConsumer
+
+TOPIC_NAME = "thing2_5_orders"
+
+consumer = KafkaConsumer(
+    TOPIC_NAME,
+    bootstrap_servers=f"kafka-1a00c87e-aiven-poc.aivencloud.com:14731",
+    client_id = "CONSUMER_CLIENT_ID",
+    group_id = "CONSUMER_GROUP_ID",
+    security_protocol="SSL",
+    ssl_cafile="ca.pem",
+    ssl_certfile="service.cert",
+    ssl_keyfile="service.key",
+)
+
+while True:
+    for message in consumer.poll().values():
+        print("Got message using SSL: " + message[0].value.decode('utf-8'))
